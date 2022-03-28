59dbe7ff173c96e52d7007f1be6cc6af809840fc
aczarnowski <adriannnczarnowski@gmail.com>
2022-03-28 17:36:18 +0200

    init

--- src/main/java/pages/OrderChart.java
+++ src/main/java/pages/OrderChart.java
@@ -11,7 +11,7 @@ public class OrderChart extends BasePage {
     private static Logger log = LoggerFactory.getLogger("OrderChart2.class");
 
     public void orderChart() {
-        ((JavascriptExecutor)driver).executeScript("scroll(0,5500)");
+//        ((JavascriptExecutor)driver).executeScript("scroll(0,5500)");
         WebElement element = driver.findElement(By.cssSelector("#add_to_cart > button > span"));
         element.click();
     }
