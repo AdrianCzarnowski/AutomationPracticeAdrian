eccfc979129c1f7527b28c5124b2873512066517
aczarnowski <adriannnczarnowski@gmail.com>
2022-03-28 17:37:30 +0200

    without scroll

--- src/main/java/pages/SummaryPage.java
+++ src/main/java/pages/SummaryPage.java
@@ -11,8 +11,8 @@ public class SummaryPage extends BasePage {
     private static Logger log = LoggerFactory.getLogger("SummaryPage.class");
 
     public void summaryPage () {
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+//        JavascriptExecutor js = ((JavascriptExecutor) driver);
+//        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         WebElement element = driver.findElement(By.cssSelector("a.button.btn.btn-default.standard-checkout"));
         element.click();
     }
